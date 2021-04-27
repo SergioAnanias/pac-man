2ae7a9a2ed98cf56313ba339b7362463e70e9ed8
Sergio Ananias <sergio.ananias.o@gmail.com>
2021-04-27 10:17:44 -0400

    Agregado cherries y segundo nivel con problemas!

--- script.js
+++ script.js
@@ -15,28 +15,74 @@ let world = [
     [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
     [0,1,1,1,1,1,0,0,0,1,1,1,1,1,0],
     [0,1,1,1,1,1,0,0,0,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,3,1,1,1,1,1,1,1,1,1,1,1,3,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
+let world2 = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,2,1,1,1,1,1,0,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,0,0,0,1,0,1,1,1,0],
+    [0,1,1,1,1,1,0,0,0,1,0,1,1,1,0],
+    [0,1,1,1,1,1,1,0,3,3,0,1,1,1,0],
+    [0,1,1,1,1,1,1,0,3,3,0,1,1,1,0],
+    [0,1,1,1,1,1,0,0,0,0,0,1,1,1,0],
+    [0,1,0,0,0,0,0,0,0,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,0,1,1,1,1,1,1,0],
+    [0,1,0,0,0,1,1,0,0,0,0,0,0,1,0],
+    [0,1,0,1,0,0,0,0,0,1,1,0,1,1,0],
+    [0,1,0,1,1,1,0,0,0,1,0,0,1,1,0],
+    [0,3,1,1,1,1,1,1,1,1,1,1,1,3,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+];
+
+
 
 var chomp = {
     x: 1,
     y: 1
 };
-var score= 0;
+let score= 0;
+let coins = 0;
+world.map(function (row) {
+    row.map(function(num) {
+        if (num == 1) { coins++}
+    })
+});
+console.log('monedas:', coins);
+
 function displayworld(){
     var output ='';
-
     for(var i=0;i<world.length;i++){
         output+="<div class='row'>\n"
         for(var j=0;j<world[i].length;j++)
         {
-            if(world[i][j]==0)
+            if(world[i][j]==0)                              //brick
                 output += "<div class='brick'></div>";
-            else if(world[i][j]==1)
+            else if(world[i][j]==1)                         //coin
                 output += "<div class='coin'></div>";
-            if(world[i][j]==2)
+            if(world[i][j]==2)                              //empty
                 output+="<div class='empty'></div>"
+            else if(world[i][j]==3)                         //cherry
+                output+="<div id='cherry'></div>"
+        }     
+        output += "</div>"
+    }
+    document.getElementById('world').innerHTML = output;
+}
+function displayworld2(){
+    var output ='';
+
+    for(var i=0;i<world2.length;i++){
+        output+="<div class='row'>\n"
+        for(var j=0;j<world2[i].length;j++){
+            if(world2[i][j]==0)
+                output += "<div class='brick'></div>";
+            else if(world2[i][j]==1)
+                output += "<div class='coin'></div>";
+            if(world2[i][j]==2)
+                output+="<div class='empty'></div>"
+            else if(world2[i][j]==3)
+                output+="<div id='cherry'></div>"
         }     
         output += "</div>"
     }
@@ -75,9 +121,23 @@ document.onkeydown = function(k){
     if(world[chomp.y][chomp.x]==1){
         world[chomp.y][chomp.x]=2;
         score +=100;
+        coins--
         displayScore();
         displayworld();
     }
+
+    else if(world[chomp.y][chomp.x]==3){
+        world[chomp.y][chomp.x]=2
+        score +=1000;
+        displayworld();
+        displayScore();
+    }
     displayworld();
+    if(coins==0){
+        console.log("Ganaste!!!");
+        displayworld2();
+    }
     displayChomp();
 }
+
+
